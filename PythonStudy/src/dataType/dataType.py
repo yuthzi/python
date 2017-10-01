adeae4eed8d6084adea3e229638456a03b83558f
yuthzi <yuth89@163.com>
2017-10-01 11:26:35 +0800

    Python学习时编写的代码

---
+++ PythonStudy/src/dataType/dataType.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+Created on 2017年9月29日
+
+@author: Administrator
+'''
+
+import math
+
+def test(a):
+    print a
+    
+if __name__ == '__main__':
+    test(123)
+    print math.ceil(1.2)
+    
---
+++ PythonStudy/src/exception/ExceptionDemo.py
@@ -0,0 +1,8 @@
+'''
+Created on 2017年10月1日
+
+@author: Administrator
+'''
+
+if __name__ == '__main__':
+    pass
